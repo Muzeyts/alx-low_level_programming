c3a195cd479a0576493cb419300e776e15894309
Muzey <muzeyts@gmail.com>
2023-02-16 12:42:48 -0800

    add everthing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
-/*
+
+/**
+ * Main - is a function
  * Use the function puts
  * I am not allowed to use printf
-*/
- int main(void)
+ * Return: always return 0 or success
+ */
+ 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /*
+ * Main - is a must 
  * Use the function printf
  * Prints "with proper grammar, but the outcome is a piece of art,"
- * our program should return 0
+ * Return: the program should return 0
  */
 int main(void)
 {
