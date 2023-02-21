f056ff6fd2b15385e1da26818826a068feacc97b
Muzey <muzeyts@gmail.com>
2023-02-21 09:16:03 -0800

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this prints the alphabets in lowercase ten times
+ *
+ * Return: Always return to 0 (Succes)
+ */
+void print_alphabet_x10(void)
+{
+	int m,n;
+	for (m = 0; m < 10; m++)
+	{
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	}
+}
