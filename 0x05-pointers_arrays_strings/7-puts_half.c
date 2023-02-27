f08feab51d776e2a2c679eadcfb31dc51341177b
Muzey <muzeyts@gmail.com>
2023-02-27 12:48:54 -0800

    prints second half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, max;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	max = i;
+/*
+	if (max % 2 == 1)
+		j = max / 2;
+	else
+		j = (max - 1) / 2;
+*/
+	j =  max / 2;
+	while (j <= max)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
